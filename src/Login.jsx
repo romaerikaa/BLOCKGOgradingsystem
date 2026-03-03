e338e809c41d8dfa757209f84f63e7db2c9238e0
romaerikaa <romaerikaa@gmail.com>
2026-03-03 15:14:16 +0800

    ADDED HELLO

--- src/Login.jsx
+++ src/Login.jsx
@@ -29,7 +29,7 @@ const Login = ({ onLogin }) => { // Accept onLogin prop from App.js
         <div className="login-card">
           <img src={plvlogo} alt="PLV Logo" className="plv-logo" />
           
-          <h2 className="welcome-text">Welcome</h2>
+          <h2 className="welcome-text"> HELLO Welcome</h2>
           
           {/* Added onSubmit handler */}
           <form className="login-form" onSubmit={handleSubmit}>
